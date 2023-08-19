66619d7f20ec20e982f8bbbf55c1e4b5da1e2fab
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-08-19 11:51:43 +0200

    creato routing per single-page-accommodation

--- src/app/Components/pages/single-page-accommodation/single-page-accommodation.component.ts
+++ src/app/Components/pages/single-page-accommodation/single-page-accommodation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AccommodationService } from '../../services/accommodation.service';
 
 @Component({
   selector: 'app-single-page-accommodation',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
 })
 export class SinglePageAccommodationComponent {
   
+  constructor(private accommodationService: AccommodationService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
