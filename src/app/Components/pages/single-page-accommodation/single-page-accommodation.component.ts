027f5041b3d40fb864b8f2420b74717b6b2e5550
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-08-19 10:49:30 +0200

    creato il componente single-page-accommodation

---
+++ src/app/Components/pages/single-page-accommodation/single-page-accommodation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-page-accommodation',
+  templateUrl: './single-page-accommodation.component.html',
+  styleUrls: ['./single-page-accommodation.component.css']
+})
+export class SinglePageAccommodationComponent {
+  
+
+}
