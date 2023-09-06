6b4fdb7f1f4754bd7e35e9e0bb20a3f968046d01
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-09-06 09:27:33 +0200

    accomodation service migliorato per salavare json nella cache

--- src/app/Components/services/accommodation.service.ts
+++ src/app/Components/services/accommodation.service.ts
@@ -1,50 +1,32 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable, map } from 'rxjs';
-// import { Accommodation } from '../models/accommodation';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AccommodationService {
-
-//   constructor(private http: HttpClient) { }
-
-//   getAccommodations(): Observable<Accommodation[]> {
-//     return this.http.get<Accommodation[]>(`assets/accommodations.json`);
-//   }
-
-//   getAccommodationByMetaUrl(metaUrl: string): Observable<Accommodation> {
-//     return this.http.get<Accommodation[]>(`assets/accommodations.json`).pipe(
-//       map((accommodations: any[]) => accommodations.find(a => a.meta_it_url === metaUrl))
-//     );
-//   }
-
-// }
-
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, shareReplay } from 'rxjs';
 import { Accommodation } from '../models/accommodation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccommodationService {
+  private cache$: Observable<Accommodation[]> | undefined;
 
   constructor(private http: HttpClient) { }
 
   getAccommodations(): Observable<Accommodation[]> {
-    return this.http.get<Accommodation[]>('https://data.krossbooking.com/get/get-apartments?id=affittasardegna&token=457c445d46733e5ae5c910b0d4e935d1');
-    
+    if (!this.cache$) {
+      this.cache$ = this.requestAccommodations().pipe(
+        shareReplay(1)
+      );
+    }
+    return this.cache$;
+  }
+
+  private requestAccommodations(): Observable<Accommodation[]> {
+    return this.http.get<Accommodation[]>('https://data.krossbooking.com/get/get-apartments?id=affittasardegna&token=457c445d46733e5ae5c910b0d4e935d1'); /*`assets/accommodations.json*/
   }
 
   getAccommodationByMetaUrl(metaUrl: string): Observable<Accommodation> {
-    return this.http.get<Accommodation[]>('https://data.krossbooking.com/get/get-apartments?id=affittasardegna&token=457c445d46733e5ae5c910b0d4e935d1').pipe(
-    map((accommodations: any[]) => accommodations.find(a => a.meta_it_url === metaUrl))
+    return this.getAccommodations().pipe(
+      map((accommodations: any[]) => accommodations.find(a => a.meta_it_url === metaUrl))
     );
   }
-
 }
