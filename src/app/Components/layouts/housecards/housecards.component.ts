20dbdeadc80a3b3745fa92c6f2fadcce90a97435
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-09-11 13:45:57 +0200

    aggiunte icone dotazioni, aggiunta icona whatsapp, fixato piccoli problemi

--- src/app/Components/layouts/housecards/housecards.component.ts
+++ src/app/Components/layouts/housecards/housecards.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Accommodation } from '../../models/accommodation';
-import { faBathtub, faBed, faWifi, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faBathtub, faBed, faWifi, faLocationDot, faMosquitoNet, faShower, faSnowflake, faKitchenSet, faTableTennis, faBicycle, faSeedling, faSquareParking, faPaw, faWaterLadder, faTv, faWarehouse} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgFor, SlicePipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
@@ -17,6 +17,18 @@ export class HousecardsComponent {
   faBed = faBed;
   faWifi = faWifi;
   faLocationDot = faLocationDot;
+  faMosquitoNet = faMosquitoNet;
+  faShower = faShower;
+  faSnowflake = faSnowflake;
+  faKitchenSet = faKitchenSet;
+  faTableTennis = faTableTennis;
+  faBicycle = faBicycle;
+  faSeedling = faSeedling;
+  faSquareParking = faSquareParking;
+  faPaw = faPaw;
+  faWaterLadder = faWaterLadder;
+  faTv = faTv;
+  faWarehouse = faWarehouse;
   @Input() public accommodations?: Accommodation[];
   public cardsToShow: number = 40;
 
@@ -30,6 +42,54 @@ export class HousecardsComponent {
 
   hasWifi(accommodation: any): boolean {
     return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Connessione WiFi');
+  }
+
+  hasMosquitoNets(accommodation: any): boolean {
+    return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Zanzariere in tutta la struttura');
+  }
+
+  hasShower(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Doccia');
+  }
+
+  hasAirConditioning(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Aria condizionata');
+  }
+
+  hasKitchen(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Cucina');
+  }
+
+  hasPingPongTable(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Tavolo da ping pong');
+  }
+
+  hasBicycle(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Bicicletta');
+  }
+
+  hasGarden(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Giardino');
+  }
+
+  hasParking(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Parcheggio');
+  }
+
+  hasPetsAllowed(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Animali domestici permessi');
+  }
+
+  hasPrivatePool(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Piscina privata');
+  }
+
+  hasTV(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'TV');
+  }
+
+  hasGarage(accommodation: any): boolean {
+      return accommodation.amenities.some((amenity: any) => amenity.name.it === 'Garage');
   }
 
 
