3b5d70fb404d60f623a06907aa9d0e041af143c1
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-09-19 10:56:40 +0200

    terminata traduzione termini e condizioni, e globale

--- src/app/Components/pages/terms/terms.component.ts
+++ src/app/Components/pages/terms/terms.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 
 @Component({
     selector: 'app-terms',
     templateUrl: './terms.component.html',
     styleUrls: ['./terms.component.css'],
-    standalone: true
+    standalone: true,
+    imports: [TranslateModule, CommonModule]
 })
 export class TermsComponent {
 
