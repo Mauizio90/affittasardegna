371eb41fa48c93eb6c206e56475d2701c7bd5a8b
Maurizio Mastropasqua <mauizio90@gmail.com>
2023-10-15 14:25:28 +0200

    meta tags

--- src/app/Components/pages/single-page-accommodation/single-page-accommodation.component.ts
+++ src/app/Components/pages/single-page-accommodation/single-page-accommodation.component.ts
@@ -12,11 +12,11 @@ import { SeoService } from '../../services/seo.service';
 
 
 @Component({
-    selector: 'app-single-page-accommodation',
-    templateUrl: './single-page-accommodation.component.html',
-    styleUrls: ['./single-page-accommodation.component.css'],
-    standalone: true,
-    imports: [NgFor, FontAwesomeModule, TranslateModule]
+  selector: 'app-single-page-accommodation',
+  templateUrl: './single-page-accommodation.component.html',
+  styleUrls: ['./single-page-accommodation.component.css'],
+  standalone: true,
+  imports: [NgFor, FontAwesomeModule, TranslateModule]
 })
 export class SinglePageAccommodationComponent {
   bigImageSource!: string;
@@ -29,7 +29,7 @@ export class SinglePageAccommodationComponent {
   faCheck = faCheck;
   faUser = faUser;
 
-  constructor(private accommodationService: AccommodationService, private route: ActivatedRoute, private titleService: Title, private metaTagService: Meta, private translate: TranslateService, private router: Router, private seo : SeoService) {}
+  constructor(private accommodationService: AccommodationService, private route: ActivatedRoute, private titleService: Title, private metaTagService: Meta, private translate: TranslateService, private router: Router, private seo: SeoService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -43,39 +43,34 @@ export class SinglePageAccommodationComponent {
               this.router.navigate(['/' + translatedPath]);
               return;
             }
-  
+
             this.accommodation = accommodation;
-            this.translate.get('singlePageMetaTitle').subscribe((str: string) => {
-              this.seo.updateTitle("AffittaSardegna - " + this.accommodation?.name_it?.toString() + str + this.accommodation?.city?.toString());
-            });
-            this.translate.get(['singlePageMetaDescription1', 'singlePageMetaDescription2', 'singlePageMetaDescription3']).subscribe((translations: { [key: string]: string }) => {
-              const str1 = translations['singlePageMetaDescription1'];
-              const str2 = translations['singlePageMetaDescription2'];
-              const str3 = translations['singlePageMetaDescription3'];
-  
-              this.seo.updateDescription(this.accommodation?.name_it?.toString() + str1 + this.accommodation?.guests + str2 + this.accommodation?.city?.toString() + str3);
-            });
-  
+            let translatedTitle = this.translate.instant('singlePageMetaTitle');
+            let translatedDescription = this.translate.instant('singlePageMetaDescription1') + this.accommodation?.name_it?.toString() + this.translate.instant('singlePageMetaDescription2') + this.accommodation?.guests + this.translate.instant('singlePageMetaDescription3') + this.accommodation?.city?.toString();
+
+            this.seo.updateTitle(translatedTitle);
+            this.seo.updateDescription(translatedDescription);
+
             if (accommodation.images && accommodation.images.length > 0) {
               const firstImage = accommodation.images[0];
               this.seo.updateImage(firstImage);
             }
-  
+
             if (accommodation.images && accommodation.images.length > 0) {
               this.bigImageSource = accommodation.images[0];
             }
-  
+
             console.log(this.accommodation);
           });
       }
     });
   }
-  
-  
+
+
 
   replaceBigImage(imageSource: string) {
     this.bigImageSource = imageSource;
   }
-  
+
 
 }
